e16b17a663745ff45bb1dd11256af96a47248b62
Ismael <ismael@MacBook-Air-de-Ismael.local>
2022-08-19 17:27:21 -0500

    update-use-hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,52 @@ import useFeth from './hooks/useFeth'
 import './App.css'
 import LocationInfo from './Components/LocationInfo'
 import CardResident from './Components/CardResident'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  
+  const [location, setLocation] = useState()
+  //estado para guardar la inf de busqueda
+  //busqueda por input
+  const [searchInput, setSearchInput] = useState('')
   //obtener location random entre 1 y 126 location
-  const randomLocation = Math.floor(Math.random() * (126 - 1) + 1)
-  
-  const URL = `https://rickandmortyapi.com/api/location/${randomLocation}`
+  //const randomLocation = Math.floor(Math.random() * (126 - 1) + 1)
 
-  const location = useFeth(URL)
-
+  //const location = useFeth(URL)
   console.log(location)
+
+  useEffect(() => {
+    let numberLocation
+
+    if(searchInput === ''){
+      numberLocation = Math.floor(Math.random() * (126 - 1) + 1)
+    }else{
+      numberLocation = searchInput
+    }
+    const URL = `https://rickandmortyapi.com/api/location/${numberLocation}`
+    axios.get(URL)
+      .then(res => setLocation(res.data))
+      .catch(err => console.log(err))
+  }, [searchInput])
+  
+
+  const handleSubmit = e => {
+    //para evitar que la pag se cargue de new
+    e.preventDefault()
+    setSearchInput(e.target.search.value)
+  }
+
+  console.log(searchInput)
 
   return (
     <div className="App">
       <h1>App Rick and Morthy</h1>
       <LocationInfo location={location} />
+      <form onSubmit={handleSubmit}>
+        <input id='search' type="text" />
+        <button>Search</button>
+      </form>
+      
       <div>
         {
           location?.residents.map(url => (
--- src/Components/CardResident.jsx
+++ src/Components/CardResident.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import useFeth from '../hooks/useFeth'
 
 const CardResident = ({url}) => {
     //recibimos las url de la api
+    const resident = useFeth(url)
+    console.log(resident)
+    
   return (
     <div>
-        {url}
+        <article>
+          <header>
+            <img src={resident?.image} alt={`image of ${resident?.name}`} />
+            <div>
+              <div></div>
+              <p>{resident?.status}</p>
+            </div>
+          </header>
+          <div>
+            <h3>{resident?.name}</h3>
+            <ul>
+              <li><span>Specie:</span>{resident?.species}</li>
+              <li><span>Origin:</span>{resident?.origin.name}</li>
+              <li><span>Episodes where appear:</span>{resident?.episode.length}</li>
+            </ul>
+          </div>
+        </article>
     </div>
   )
 }
